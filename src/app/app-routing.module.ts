b823a035e0712c1fa035b1cde9d924f7869d8ab4
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 14:28:02 -0600

    confiurando app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUpdateProductionComponent } from './components/create-update-production/create-update-production.component';
+import { DeleteProductionComponent } from './components/delete-production/delete-production.component';
+import { ListProductionComponent } from './components/list-production/list-production.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'list',component:ListProductionComponent},
+  {path:'create',component:CreateUpdateProductionComponent},
+  {path:'update',component:CreateUpdateProductionComponent},
+  {path:'delete',component:DeleteProductionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
