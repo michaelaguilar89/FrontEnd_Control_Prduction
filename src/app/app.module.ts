cf5b7a9f5d64e90fc6b533f5fba1b539e64c72bc
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 14:14:37 -0600

    generamos footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+//import { HeaderComponent } from './components/header/header.component';
 import { ListProductionComponent } from './components/list-production/list-production.component';
 import { CreateUpdateProductionComponent } from './components/create-update-production/create-update-production.component';
 import { DeleteProductionComponent } from './components/delete-production/delete-production.component';
 import { GetByIdProductionComponent } from './components/get-by-id-production/get-by-id-production.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+   // HeaderComponent,
     ListProductionComponent,
     CreateUpdateProductionComponent,
     DeleteProductionComponent,
-    GetByIdProductionComponent
+    GetByIdProductionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
