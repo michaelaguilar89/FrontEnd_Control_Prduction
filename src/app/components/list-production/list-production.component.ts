f4a5801d1b02da5ec63fd1a1a91b992b28707a0b
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-24 10:19:28 -0600

    actualizando el metodo getByDate

--- src/app/components/list-production/list-production.component.ts
+++ src/app/components/list-production/list-production.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
-
+import { Consumo } from 'src/models/consumo';
+import { ProductionService } from 'src/app/services/production.service';
+import { FormGroup,FormBuilder ,Validators} from '@angular/forms';
 @Component({
   selector: 'app-list-production',
   templateUrl: './list-production.component.html',
   styleUrls: ['./list-production.component.css']
 })
 export class ListProductionComponent {
+  form:FormGroup;
+
+  constructor(public service:ProductionService,
+              private fb:FormBuilder){
+      this.form=this.fb.group({
+        searchDate:['',Validators.required]
+    })
+
+}
+  
+ngOnInit():void{
+  var date=new Date();
+  var today= date.toDateString();
+  var time = date.toTimeString();
+  
+   var fecha=new Date(today);
+   console.log('today : '+today);
+   console.log('fecha : '+fecha);
+ this.service.getConsumobyDate(today);
+
+}  getByDate(){
+  var a= this.form.get('searchDate')?.value;
+  alert('fecha de busqueda : '+a);
+   this.service.getConsumobyDate(a);
+  console.log(this.service.list.result);
+  console.log('DisplayMessages:'+this.service.list.DisplayMessage)
+
+}
+  getAll(){
+    this.service.getConsumo();
+    console.log(this.service.list.result);
+
+  }
+  new(){
+
+  }
+
+  edit(product:Consumo){
+
+  }
 
 }
--- src/app/services/production.service.ts
+++ src/app/services/production.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Consumo } from 'src/models/consumo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductionService {
+ 
+  myappurl:string ="https://localhost:5001/api/Production/";
+  list:any=[];
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getConsumo(){
+    this.http.get(this.myappurl).toPromise().then(data=>{
+      this.list=data 
+    })
+  }
+  getConsumobyDate(fecha:string){
+    this.http.get(this.myappurl+fecha).toPromise().then(data=>{
+      this.list=data;
+    })
+  }
+
 }
