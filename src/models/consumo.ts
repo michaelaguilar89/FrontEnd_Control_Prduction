e272d905c42e310c887375f0a7a4f75d149e5809
michaelaguilar89 <mchlaguilar051@hotmail.es>
2023-01-23 21:44:08 -0600

    actualizando el modelo

--- src/models/consumo.ts
+++ src/models/consumo.ts
@@ -1,3 +1,13 @@
 export class Consumo{
-    
+    id?:number;
+    fecha!: Date;
+    tanque!: string;
+    productoName!: string;
+    lote!: string;
+    cantidad!: number;
+    horaInicio!: string;
+    nivelFinal!: string;
+    responsable!: string;
+    comentario!: string;
+
 }
